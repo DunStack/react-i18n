08aaa6bafac423862283e64f610d1c648b21f796
colbychounm <nmc.1909@gmail.com>
2024-08-04 17:00:10 +0700

    chore: init vitest

---
+++ lib/__tests__/main.test.ts
@@ -0,0 +1,3 @@
+it("should be true", () => {
+  expect(1 + 1).toBe(2);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  test: {
+    globals: true,
+  },
   build: {
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter'
-    }
-  }
-})
+      entry: "./lib/main.ts",
+      name: "Counter",
+      fileName: "counter",
+    },
+  },
+});
